5395b02068928feb853e36e3ca1db829ebb5d36f
olafpapa <olaf0daisuki>
2019-07-07 17:38:55 +0900

    ローカルストレージにstateをまるごと保存する。

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers'
 import EventForm from './EventForm'
@@ -6,19 +6,27 @@ import EventList from './EventList'
 import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 
+const LOCAL_STORAGE_KEY = 'data'
+
 const App = () => {
-  const initialState = {
+  const savedState = localStorage.getItem(LOCAL_STORAGE_KEY)
+  const initialState = savedState ? JSON.parse(savedState) : {
     events: [],
     operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
+    }, [state]
+  )
 
   return (
     <AppContext.Provider value={ {state, dispatch} }>
       <div className="container-fluid">
         <EventForm/>
         <EventList/>
-        <OperationLogs />
+        <OperationLogs/>
       </div>
     </AppContext.Provider>
 
